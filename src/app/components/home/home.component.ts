75a6a002548ebea84a5be5a5e77b0533f5470596
Marisanti <snunyez@mariabarcelona.com>
2018-01-16 15:59:33 +0100

    prueba 03

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,64 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {}
+  constructor(){
+	
+	}
 
   ngOnInit() {
 
-  	/*===== EVENTOS - INICIO =====*/
+		/*===== EVENTOS - INICIO =====*/
 
-	//Eventos que activan el OnePageScroll
-	document.addEventListener("swipeUp", function(event){
-  		moveDown();
-  	})
-  	document.addEventListener("swipeDown", function(event){
-  		moveUp();
-  	})
-  	document.addEventListener("teclaUp", function(event){
-  		moveUp();
-  	})
-  	document.addEventListener("teclaDown", function(event){
-  		moveDown();
-  	})
-  	document.addEventListener("ruedaUp", function(event){
-  		moveUp();
-  	})
-  	document.addEventListener("ruedaDown", function(event){
-  		moveDown();
-  	})
-  	window.addEventListener('resize', function(event){
-    	moveResize();
-    })
+		//Eventos que activan el OnePageScroll
+		document.addEventListener("swipeUp", function(event){
+			moveDown();
+		})
+		document.addEventListener("swipeDown", function(event){
+			moveUp();
+		})
+		document.addEventListener("teclaUp", function(event){
+			moveUp();
+		})
+		document.addEventListener("teclaDown", function(event){
+			moveDown();
+		})
+		document.addEventListener("ruedaUp", function(event){
+			moveUp();
+		})
+		document.addEventListener("ruedaDown", function(event){
+			moveDown();
+		})
+		window.addEventListener('resize', function(event){
+			moveResize();
+		})
 
-	//Eventos para scroll de raton
-	document.addEventListener("wheel", function(e){
-		if (e.deltaY < 0) {
-			var event:any = new Event('ruedaUp');
-  			document.dispatchEvent(event);
-		}
-		if (e.deltaY > 0) {
-			var event:any = new Event('ruedaDown');
-  			document.dispatchEvent(event);
-		}
-	})
+		//Eventos para scroll de raton
+		document.addEventListener("wheel", function(e){
+			if (e.deltaY < 0) {
+				var event:any = new Event('ruedaUp');
+					document.dispatchEvent(event);
+			}
+			if (e.deltaY > 0) {
+				var event:any = new Event('ruedaDown');
+					document.dispatchEvent(event);
+			}
+		})
 
-	//Eventos para teclas de flecha arriba/abajo
-	document.addEventListener("keydown", function(e){
-		var keyCode = e.keyCode;
-		if (keyCode == 38) {
-    		var event:any = new Event('teclaUp');
-  			document.dispatchEvent(event);
-		}
-		if (keyCode == 40){
-			var event:any = new Event('teclaDown');
-  			document.dispatchEvent(event);
-		}
-	})
+		//Eventos para teclas de flecha arriba/abajo
+		document.addEventListener("keydown", function(e){
+			var keyCode = e.keyCode;
+			if (keyCode == 38) {
+					var event:any = new Event('teclaUp');
+					document.dispatchEvent(event);
+			}
+			if (keyCode == 40){
+				var event:any = new Event('teclaDown');
+					document.dispatchEvent(event);
+			}
+		})
 
   	//Eventos para movil (swipe Up y Down)
-  	var startX
-  	var	startY
-  
+		var startX, startY
+		
     document.addEventListener("touchstart", touchstart);  
 	  
   	function touchstart(event) {
@@ -79,7 +80,7 @@ export class HomeComponent implements OnInit {
   	function touchmove(event) {
   		var touches = event.touches;
   		if (touches && touches.length) {
-  		  	//event.preventDefault();
+  		  event.preventDefault();
   			var deltaX = startX - touches[0].pageX;
   			var deltaY = startY - touches[0].pageY;
   
@@ -92,7 +93,8 @@ export class HomeComponent implements OnInit {
   			  var event:any = new Event('swipeRight');
   			  document.dispatchEvent(event);
   			}
-  			*/
+				*/
+				
   			if (deltaY >= 50) {
   			  var event:any = new Event('swipeUp');
   			  document.dispatchEvent(event);
@@ -109,28 +111,40 @@ export class HomeComponent implements OnInit {
   	}
   	/*===== EVENTOS - FIN =====*/
 
-  	function moveResize(){
-  		var a:number = Number(document.querySelector("main.home").getAttribute("data-step"))
-  		moveTo(a);
-  	}
+		function moveResize(){
+			var a:number = Number(document.querySelector("main.home").getAttribute("data-step"))
+			moveTo(a, false);
+		}
 
-  	function moveDown(){
-  		var a:number = Number(document.querySelector("main.home").getAttribute("data-step"))
-  		var c:number = document.querySelectorAll("main.home > section").length
-  		if( a < c){ moveTo(a+1) }
-	}
+		function moveDown(){
+			var a:number = Number(document.querySelector("main.home").getAttribute("data-step"))
+			var c:number = document.querySelectorAll("main.home > section").length
+			if( a < c){ moveTo(a+1) }
+		}
 
-	function moveUp(){
-		var a:number = Number(document.querySelector("main.home").getAttribute("data-step"))
-  		if( a > 1){ moveTo(a-1) }
-	}
+		function moveUp(){
+			var a:number = Number(document.querySelector("main.home").getAttribute("data-step"))
+				if( a > 1){ moveTo(a-1) }
+		}
 
-	function moveTo(d){
-		var h = window.innerHeight;
-		var elemento = <HTMLElement>document.querySelector("main.home");
-		elemento.setAttribute("data-step",d)
-		elemento.style.transform = `translate3d(0,-${h*(d-1)}px,0)`;
-	}
+		var tiempo = false;
+		function moveTo(d, t?){
+			if (t != undefined){tiempo = t}
+			if(tiempo == false){
+				var h = window.innerHeight;
+				var elemento = <HTMLElement>document.querySelector("main.home");
+				elemento.setAttribute("data-step",d)
+				elemento.style.transform = `translate3d(0,-${h*(d-1)}px,0)`;
+				tiempo = true;
+				setTimeout(function(){tiempo = false}, 700)
+			}
+		}
+
+		/* Crear menu de bolas */
+		function menuLateral(){
+			var diapositivas:number = document.querySelectorAll("main.home > section").length
+			console.log(diapositivas)
+		}
 
   }//fin ngOnInit
 
