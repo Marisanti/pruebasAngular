3021aab10d93b6502f501fc24a77c36138b582a0
Marisanti <snunyez@mariabarcelona.com>
2018-01-23 16:41:17 +0100

    cambios estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/elements/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { FichaComponent } from './components/ficha/ficha.component';
 
--- src/app/components/ficha/ficha.component.ts
+++ src/app/components/ficha/ficha.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ficha',
-  templateUrl: './ficha.component.html',
-  styleUrls: ['./ficha.component.css']
+  templateUrl: './ficha.component.html'
 })
 export class FichaComponent implements OnInit {
 
